fb5083f1fc49d58071aa86445cbfcdc994721aba
Sima Halwany <simahalwany@gmail.com>
2020-11-20 22:36:07 +0200

    done

--- app/assets/config/routes.rb
+++ app/assets/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'ask',    to: 'questions#ask',    as: :ask
-  get 'answer', to: 'questions#answer', as: :answer
+  get '/ask',    to: 'questions#ask',    as: :ask
+
+  get '/answer', to: 'questions#answer', as: :answer
 end
